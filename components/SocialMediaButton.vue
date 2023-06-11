faf4d541734078e46017da6c51121e0d8490a6da
D'Bonis <dev@rodolfodebonis.com.br>
2023-06-11 14:45:55 -0300

    feat: added SocialMediaButton component and changed text from main page

---
+++ components/SocialMediaButton.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+// eslint-disable-next-line import/named
+  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+  export default {
+    components: { FontAwesomeIcon },
+    props: {
+      link: {
+        type: String,
+        required: false,
+        default: ""
+      },
+      icon: {
+        type: Array<String>,
+        required: true,
+        default: ['fab', 'github']
+      }
+
+    }
+  }
+</script>
+
+<template>
+  <a :href="link" target="_blank">
+    <font-awesome-icon
+      class="text-white fa-lg px-3"
+      :icon="icon"
+    />
+  </a>
+</template>
