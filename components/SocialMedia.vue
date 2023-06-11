1de8209b7e6b52008ec7ec2d9966c98b615786d5
D'Bonis <dev@rodolfodebonis.com.br>
2023-06-11 14:50:10 -0300

    fix: fix lint issues

--- components/SocialMedia.vue
+++ components/SocialMedia.vue
@@ -1,20 +1,31 @@
 <template>
   <div>
     <client-only>
-      <SocialMediaButton :icon="['fab', 'github']" link="https://github.com/RodolfoBonis"/>
-      <SocialMediaButton :icon="['fab', 'linkedin']" link="https://www.linkedin.com/in/rodolfo-de-bonis/"/>
-      <SocialMediaButton :icon="['fab', 'twitter']" link="https://twitter.com/RodolfoBonis"/>
-      <SocialMediaButton :icon="['fas', 'envelope']" link="mailto:dev@rodolfodebonis.com.br"/>
+      <SocialMediaButton
+        :icon="['fab', 'github']"
+        link="https://github.com/RodolfoBonis"
+      />
+      <SocialMediaButton
+        :icon="['fab', 'linkedin']"
+        link="https://www.linkedin.com/in/rodolfo-de-bonis/"
+      />
+      <SocialMediaButton
+        :icon="['fab', 'twitter']"
+        link="https://twitter.com/RodolfoBonis"
+      />
+      <SocialMediaButton
+        :icon="['fas', 'envelope']"
+        link="mailto:dev@rodolfodebonis.com.br"
+      />
     </client-only>
   </div>
 </template>
 
 <script>
-
-import {defineComponent} from "vue";
-import SocialMediaButton from "~/components/SocialMediaButton.vue";
+import { defineComponent } from 'vue'
+import SocialMediaButton from '~/components/SocialMediaButton.vue'
 
 export default defineComponent({
-  components: {SocialMediaButton}
+  components: { SocialMediaButton },
 })
 </script>
